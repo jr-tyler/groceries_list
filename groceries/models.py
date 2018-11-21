b1ad01ac4082b7178eb01f4fbfec0103cbf33222
tylerj <ropertylerj@gmail.com>
2018-11-21 14:45:12 +1300

    Add ability to add items to the grocery list from the admin interface.

--- groceries/models.py
+++ groceries/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Grocery(models.Model):
+    person = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    item = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=8, decimal_places=2)
+    bought_date = models.DateTimeField(default=timezone.now)
+    paid_date = models.DateTimeField(blank=True, null=True)
+
+    def paid(self):
+        self.paid_date = timezone.now()
+        self.save()
+
+    # instead of displaying the objects as numbers it returns the item label as the descriptor
+    def __str__(self):
+        return self.item + ' bought by ' + str(self.person)
