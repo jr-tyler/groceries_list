27d0b1a68d97a66ea7648799b098b0e0bbe0a170
tylerj <ropertylerj@gmail.com>
2018-11-22 16:28:30 +1300

    Added tables and some CSS

--- groceries/models.py
+++ groceries/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.utils import timezone
+from datetime import datetime
 
 # Create your models here.
 class Grocery(models.Model):
     person = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     item = models.CharField(max_length=100)
     cost = models.DecimalField(max_digits=8, decimal_places=2)
-    bought_date = models.DateTimeField(default=timezone.now)
-    paid_date = models.DateTimeField(blank=True, null=True)
+
+    bought_date = models.DateField(default=timezone.now)
+    paid_date = models.DateField(blank=True, null=True)
 
     def paid(self):
         self.paid_date = timezone.now()
