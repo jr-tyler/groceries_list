30b0d457a91a9fc4dd31de1e5f01e36124472009
tylerj <ropertylerj@gmail.com>
2018-11-26 17:26:31 +1300

    Added some front end CSS

--- groceries/views.py
+++ groceries/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from .models import Grocery
 
@@ -7,3 +7,9 @@ def grocery_list(request):
     groceries = Grocery.objects.filter(bought_date__lte=timezone.now()).order_by('-bought_date')
     stuff_for_front_end = {'groceries': groceries}
     return render(request, 'groceries/groceries_list.html', stuff_for_front_end)
+
+def purchaser_list(request, pk):
+
+    purchaser = get_object_or_404(Grocery, pk=pk)
+    stuff_for_front_end = {'purchaser': purchaser}
+    return render(request, 'groceries/purchaser.html', stuff_for_front_end)
